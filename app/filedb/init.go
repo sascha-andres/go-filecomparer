56cba08db03cc829e22b459eb00d7afe532171e9
Sascha Andres <sascha.andres@outlook.com>
2017-03-03 11:29:37 +0100

    Added db initialization logic

--- app/filedb/init.go
+++ app/filedb/init.go
@@ -15,7 +15,10 @@
 package filedb
 
 import (
+	"os"
+
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite" // db plugin for gorm
 )
 
 var (
@@ -25,6 +28,47 @@ var (
 	DB *gorm.DB
 )
 
-func init() {
+// Initialize called to create an empty database
+func Initialize() error {
+	if nil != DB {
+		if err := removeDatabase(); err != nil {
+			return err
+		}
+	}
+	return ConnectDB()
+}
 
+// ConnectDB opens a database and migrates
+func ConnectDB() error {
+	var err error
+	DB, err = gorm.Open("sqlite3", FileDatabasePath)
+	if err != nil {
+		DB.AutoMigrate(&File{})
+		err = DB.Error
+	}
+	return err
+}
+
+// CloseDB disconnects from database
+func CloseDB() error {
+	return DB.Close()
+}
+
+func removeDatabase() error {
+	if ok, _ := exists(FileDatabasePath); ok {
+		return os.Remove(FileDatabasePath)
+	}
+	return nil
+}
+
+// exists returns whether the given file or directory exists or not
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return true, err
 }
